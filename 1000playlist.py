7af0d501c6d53b42b2a88b4dbb17ce329d9705bb
Ghirardi Nicola <nicola.ghirardi@leitha.eu>
2023-01-14 20:11:51 +0100

    basic doc

--- 1000playlist.py
+++ 1000playlist.py
@@ -4,10 +4,9 @@ from gpt import build_playlist,build_gpt_prompt
 
 logging.basicConfig(level=logging.INFO)
 
-playlist_desc = input('Voglio una playlist _')
+playlist_desc = input('Voglio una playlist ')
 
 prompt=build_gpt_prompt(playlist_desc)
-logging.info(prompt)
 
 playlist_obj= build_playlist(prompt)
 logging.info(prompt)
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # musicMille
+
+Linux terminal command to create a musicMille spotify playlist, just as talking with (chat)GPT
+
+Create a config.yaml file such as (first entries of spotify):   
+'''
+client_id: 
+client_secret: 
+username: 
+open_api_api_key: 
+'''
