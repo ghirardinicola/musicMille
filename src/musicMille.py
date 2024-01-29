74c7020c5ad49bbb82fad6c48132be431fbd83bb
Ghirardi Nicola <nicola.ghirardi@leitha.eu>
2024-01-29 09:40:38 +0100

    back in business. It create a playlist for the coding morning

--- src/musicMille.py
+++ src/musicMille.py
@@ -8,7 +8,7 @@ import yaml
 from langchain.chains import LLMChain
 from langchain_openai import OpenAI
 from langchain.chains.conversation.memory import ConversationBufferMemory
-from langchain.agents import create_openai_functions_agent
+from langchain.agents import create_openai_functions_agent, create_react_agent
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
@@ -18,40 +18,41 @@ with open('config.yaml', 'r') as file:
     config = yaml.safe_load(file)
 
 n = len(sys.argv)
-userInput=""
-if n>1:
+userInput = ""
+if n > 1:
     for i in range(1, n):
-        userInput+=sys.argv[i] + " "
+        userInput += sys.argv[i] + " "
 else:
     userInput = "Make a mixtape " + input('''Make a mixtape ''')
 
 # Construct the agent. We will use the default agent type here.
-tools_names=[listen_tool, generatemixtape_tool, followup_tool]
+tools_names = [listen_tool, generatemixtape_tool, followup_tool]
 
 memory = ConversationBufferMemory(memory_key="chat_history")
 # , model=config["model"]
-llm=OpenAI(temperature=0.7)
+llm = OpenAI(temperature=0)
 
-impersonification='''
+impersonification = '''
 Your task is to work with the Human to create a mixtape and write it to spotify. 
-Ask followup questions to the Human in order to get a more detailed description of the mixtape you have to create. 
-Create a new version of the mixtape after the Human responses.
-Use the tool to write the mixtape to spotify, after the user confirm it.
-Remember the Human that can ask for modification of the playlist and ask followup questions.
+Ask followup questions to the Human in response, in order to get a more detailed description of the mixtape you have to create. 
+Create a refined version of the mixtape after every user input and show the user the result. 
+Use the tool to write the mixtape to spotify only when the user asks for it. 
 '''
-
-tools_prompt=""" You have access to the following tools:"""
-suffix = """Make a mixtape... "
+#Explain the user why
+#Remember the Human that can ask for modification of the playlist and ask followup questions.
+#tools_prompt = """ You have access to the following tools:"""
+suffix = """
 
 {chat_history}
-Question: {input}
+ {input}
 {agent_scratchpad}"""
 
+#create_react_agent()
 
 prompt = ConversationalAgent.create_prompt(
     tools_names,
-    prefix=impersonification+tools_prompt, 
-    suffix=suffix, 
+    prefix=impersonification,# + tools_prompt,
+    suffix=suffix,
     input_variables=["input", "chat_history", "agent_scratchpad"]
 )
 
@@ -60,10 +61,9 @@ agent = ConversationalAgent(llm_chain=llm_chain, verbose=True, return_intermedia
 agent_executor = AgentExecutor(agent=agent, tools=tools_names, verbose=True, memory=memory)
 
 while True:
-    if userInput=="exit":
+    if userInput == "exit":
         exit()
     else:
         logger.info("invoke the agent")
-        response=agent_executor.invoke({"input": userInput})
-        userInput=input("")
-
+        response = agent_executor.invoke({"input": userInput})
+        userInput = input(response["output"])
---
+++ src/musicMille_2.py
@@ -0,0 +1,87 @@
+import logging
+
+import sys
+import os
+from tools import listen_tool, generatemixtape_tool, followup_tool
+from langchain.agents import ConversationalAgent, initialize_agent, load_tools
+from langchain.agents import AgentExecutor, create_structured_chat_agent
+
+import yaml
+from langchain.chains import LLMChain
+from langchain_openai import OpenAI
+# from langchain.chains.conversation.memory import ConversationBufferMemory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+
+n = len(sys.argv)
+userInput = ""
+if n > 1:
+    for i in range(1, n):
+        userInput += sys.argv[i] + " "
+else:
+    userInput = "Make a mixtape " + input('''Make a mixtape ''')
+
+tools = [listen_tool, generatemixtape_tool, followup_tool]
+
+# memory = ConversationBufferMemory(memory_key="chat_history")
+# , model=config["model"]
+llm = OpenAI(temperature=0)
+
+impersonification = '''
+Your are MusicMille. Your task is to chat with the User to create a mixtape and then write it to spotify. 
+Ask some followup questions to the User in order to get a more detailed description of the mixtape you have to create. 
+Create a new version of the mixtape after the Human responses.
+Use the tool to write the mixtape to spotify, after the user confirm it.
+Remember the Human that can ask for modification of the playlist and ask followup questions.
+'''
+
+# tools_prompt = """ You have access to the following tools:"""
+# {chat_history}
+request = """Make a mixtape  {input} """
+
+# final_input = impersonification + " This is user input " + request
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            impersonification,
+        ),
+        ("user", request),
+        MessagesPlaceholder(variable_name="agent_scratchpad"),
+    ]
+)
+
+# prompt = ConversationalAgent.create_prompt(
+#     tools_names,
+#     prefix=impersonification + tools_prompt,
+#     suffix=suffix,
+#     input_variables=["input", "chat_history", "agent_scratchpad"]
+# )
+
+# llm_chain = LLMChain(llm=llm, prompt=prompt, verbose=True)
+
+from langchain import hub
+obj = hub.pull("hwchase17/structured-chat-agent")
+
+# Construct the JSON agent
+agent = create_structured_chat_agent(llm=llm, tools=tools, prompt=prompt)
+# agent = ConversationalAgent(llm_chain=llm_chain, verbose=True, return_intermediate_steps=True)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, max_iterations=5)  # memory=memory
+
+while True:
+    if userInput == "exit":
+        exit()
+    else:
+        logger.info("invoke the agent")
+        response = agent_executor.invoke({"input": userInput} , "chat_history": [
+            HumanMessage(content="hi! my name is bob"),
+            AIMessage(content="Hello Bob! How can I assist you today?"),
+        ],)
+        userInput = input("")
